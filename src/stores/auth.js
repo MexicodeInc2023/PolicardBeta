3d9bd9dbc3e12b589bec082f8db0292f07c2d513
JoseArjona <j.angel.matheis@gmail.com>
2023-03-12 16:33:04 -0600

    Store const act

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,8 +1,28 @@
- import { writable } from "svelte/store"; 
+import { writable } from "svelte/store"; 
+import { browser } from "$app/environment"
 
 
-export let jwt = writable(null);
-export let user = writable(null);
-export let emailUser = writable(null);
-export let id = writable(null);
-export let authenticated = writable(false);
+export const jwt = writable(
+    browser && (localStorage.getItem("jwt") || null)
+);
+jwt.subscribe((value) => browser && (localStorage.jwt = value));
+
+export const id = writable(
+    browser && (localStorage.getItem("id") || null)
+);
+id.subscribe((value) => browser && (localStorage.id = value));
+
+export const user = writable( 
+    browser && (localStorage.getItem("user") || null)
+);
+user.subscribe((value) => browser && (localStorage.user = value));
+
+export const emailUser = writable(
+    browser && (localStorage.getItem("emailUser") || null)
+);
+emailUser.subscribe((value) => browser && (localStorage.emailUser = value));
+
+export const authenticated = writable(
+    browser && (localStorage.getItem("authenticated") || false)
+);
+authenticated.subscribe((value) => browser && (localStorage.authenticated = value));
--- src/stores/states.js
+++ src/stores/states.js
@@ -1,4 +1,11 @@
 import { writable } from "svelte/store"; 
+import { browser } from "$app/environment"
+
+
+export const statusCredentials = writable(
+    browser && (localStorage.getItem("statusCredentials") || 1)
+);
+statusCredentials.subscribe((value) => browser && (localStorage.statusCredentials = value));
 
 export const count = writable(0);
-export const statusCredentials = writable(1);
+/* export const statusCredentials = writable(1); */
