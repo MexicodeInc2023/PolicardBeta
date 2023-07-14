47206c1d7db9ced904dfe44d4946ec82f9099b8a
JoseArjona <j.angel.matheis@gmail.com>
2023-07-13 23:41:23 -0600

    seguridad

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,14 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		csp: {
+			mode: "hash",
+			directives: { "script-src": ["self"] },
+			reportOnly: {
+				'script-src': ['self']
+			}
+		},
 	}
 };
 
